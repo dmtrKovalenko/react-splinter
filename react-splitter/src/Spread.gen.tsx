4b6ecccd99c4e0f230c9ffb603215a7e687608e7
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2021-04-17 19:51:35 +0300

    commit new files

---
+++ react-splitter/src/Spread.gen.tsx
@@ -0,0 +1,13 @@
+/* TypeScript file generated from Spread.res by genType. */
+/* eslint-disable import/first */
+
+
+import * as React from 'react';
+
+// tslint:disable-next-line:no-var-requires
+const SpreadBS = require('./Spread.bs');
+
+// tslint:disable-next-line:interface-over-type-literal
+export type Props<props> = { readonly children: React.ReactNode; readonly props: props };
+
+export const Spread: React.ComponentType<{ readonly children: React.ReactNode; readonly props: any }> = SpreadBS.make;
